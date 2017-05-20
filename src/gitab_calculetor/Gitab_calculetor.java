89228921561b238dd71ad2fd9c5e65c3ceb22ee3
s <s@s-PC>
2017-05-20 05:30:02 -0700

    The First Team  add another method called "deference "  used for to subtract numbers

--- src/gitab_calculetor/Gitab_calculetor.java
+++ src/gitab_calculetor/Gitab_calculetor.java
@@ -12,9 +12,31 @@ public class Gitab_calculetor {
           int c=f+g;
           JOptionPane.showMessageDialog(null,"the sum is "+c );
       }
+      public void diference(){
+      String a= JOptionPane.showInputDialog("enter the first number");
+          int f=Integer.parseInt(a);
+          String b=JOptionPane.showInputDialog("enter the Second number");
+          int g=Integer.parseInt(b);
+          int c=f+g;
+          JOptionPane.showMessageDialog(null,"the sum is "+c );
+      }
     public static void main(String[] args) {
       Gitab_calculetor Gc=new Gitab_calculetor();
-        Gc.sum();
+      String a= JOptionPane.showInputDialog("enter 1 for Adition number enter 2 for subtraction");
+       int f=Integer.parseInt(a);
+      if(f==1){
+      Gc.sum();
+      
+      }
+      else if(f==2){
+     
+      Gc.diference();    
+      }
+      else{
+      JOptionPane.showMessageDialog(null,"plese read  insraction");
+      }
+      //String b= JOptionPane.showInputDialog("enter B for Subtraction number");
+      
     }
     
 }
